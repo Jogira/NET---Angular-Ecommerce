70471a8bc66ed81a27f0453ad86c5f335cc77344
Jonathan <Zweitonegoismus@gmail.com>
2023-06-09 17:17:41 -0400

    API basics completed.

--- holonet/API/Controllers/ProductsController.cs
+++ holonet/API/Controllers/ProductsController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,18 +13,26 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
 
-        public string GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "ALL PRODUCTS";
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
 
         [HttpGet("{id}")]
-        public string GetPrdocut(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "ONE PRODUCT";
+            return await _context.Products.FindAsync(id);
         }
     }
 }
