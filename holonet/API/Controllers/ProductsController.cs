dae0b9267c6e2d9f8bd317d350a04d0bd7e9e36b
Jonathan <Zweitonegoismus@gmail.com>
2023-06-09 15:19:20 -0400

    Base files genned.

---
+++ holonet/API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+
+        public string GetProducts()
+        {
+            return "ALL PRODUCTS";
+        }
+
+
+        [HttpGet("{id}")]
+        public string GetPrdocut(int id)
+        {
+            return "ONE PRODUCT";
+        }
+    }
+}
