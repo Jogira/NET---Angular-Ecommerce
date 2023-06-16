fea563cc72a99550a9a26cb1bf4d3ca57a282a1a
Jonathan <Zweitonegoismus@gmail.com>
2023-06-16 13:54:27 -0400

    APi connected to front end and interfaces added.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Holonet';
+  products!: IProduct[];
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>('https://localhost:7093/api/products?pageSize=50').subscribe((response) => {
+      this.products = response.data;
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
