447d41999a7dda10efaf30966dbc8670e6229364
Jonathan <Zweitonegoismus@gmail.com>
2023-06-27 12:47:59 -0400

    Basket service setup.

---
+++ client/src/app/basket/basket-routing.module.ts
@@ -0,0 +1,20 @@
+import { Router, RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from './basket.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+const routes: Routes =
+  [
+    { path: '', component: BasketComponent }
+  ];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class BasketRoutingModule { }
