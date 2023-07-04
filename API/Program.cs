19a43d4efccc76915e5e83b819349542ee09d444
Jonathan <Zweitonegoismus@gmail.com>
2023-07-04 11:55:51 -0400

    Added being able to authorize via the Swagger UI.

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ using Infrastructure.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -70,7 +71,31 @@ builder.Services.AddCors(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+    {
+        var securitySchema = new OpenApiSecurityScheme
+        {
+            Description = "JWT Auth Bearer Scheme",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        };
+
+        c.AddSecurityDefinition("Bearer", securitySchema);
+        var securityRequirement = new OpenApiSecurityRequirement {{securitySchema, new[]
+        {
+            "Bearer"
+        }}};
+
+        c.AddSecurityRequirement(securityRequirement);
+    }
+);
 
 var app = builder.Build();
 
